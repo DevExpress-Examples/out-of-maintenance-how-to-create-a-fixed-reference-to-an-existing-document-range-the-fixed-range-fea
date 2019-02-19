6a4b044d769dc36c3694f7972ad92124d43f438d
DevExpressExampleBot <fakeEmail>
2019-02-19 13:16:37 +0300

    Source auto update [skip ci]

--- VB/WindowsFormsApplication1/Form1.vb
+++ VB/WindowsFormsApplication1/Form1.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-#Region "#usings"
+﻿#Region "#usings"
 Imports System
 Imports System.Drawing
 Imports DevExpress.XtraRichEdit.API.Native
@@ -8,11 +7,12 @@ Imports DevExpress.XtraRichEdit.API.Native
 Namespace WindowsFormsApplication1
 	Partial Public Class Form1
 		Inherits DevExpress.XtraBars.Ribbon.RibbonForm
+
 		Public Sub New()
 			InitializeComponent()
 		End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
 			richEditControl1.CreateNewDocument()
 		End Sub
 
@@ -35,29 +35,27 @@ Namespace WindowsFormsApplication1
 '			#End Region ' #FixedRangeUse
 		End Sub
 	End Class
-#Region "#FixedRangeExtension"
+	#Region "#FixedRangeExtension"
 	Public Module FixedRangeExtension
-        Sub New()
-        End Sub
-        <System.Runtime.CompilerServices.Extension>
-        Public Function GetFixedRange(ByVal range As DocumentRange) As CustomFixedRange
-            Return New CustomFixedRange(range)
-        End Function
-        <System.Runtime.CompilerServices.Extension>
-        Public Function BeginUpdateCharacters(ByVal document As Document, ByVal range As CustomFixedRange) As CharacterProperties
-            Return document.BeginUpdateCharacters(range.CreateRange(document))
-        End Function
-    End Module
-    Public Class CustomFixedRange
-        Private start As Integer
-        Private length As Integer
-        Public Sub New(ByVal range As DocumentRange)
-            Me.start = range.Start.ToInt()
-            Me.length = range.Length
-        End Sub
-        Public Function CreateRange(ByVal document As Document) As DocumentRange
-            Return document.CreateRange(start, length)
-        End Function
-    End Class
-#End Region ' #FixedRangeExtension
+		<System.Runtime.CompilerServices.Extension> _
+		Public Function GetFixedRange(ByVal range As DocumentRange) As CustomFixedRange
+			Return New CustomFixedRange(range)
+		End Function
+		<System.Runtime.CompilerServices.Extension> _
+		Public Function BeginUpdateCharacters(ByVal document As Document, ByVal range As CustomFixedRange) As CharacterProperties
+			Return document.BeginUpdateCharacters(range.CreateRange(document))
+		End Function
+	End Module
+	Public Class CustomFixedRange
+		Private start As Integer
+		Private length As Integer
+		Public Sub New(ByVal range As DocumentRange)
+			Me.start = range.Start.ToInt()
+			Me.length = range.Length
+		End Sub
+		Public Function CreateRange(ByVal document As Document) As DocumentRange
+			Return document.CreateRange(start, length)
+		End Function
+	End Class
+	#End Region ' #FixedRangeExtension
 End Namespace
